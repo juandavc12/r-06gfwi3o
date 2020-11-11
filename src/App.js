4947ebf12635f96b1f7267d37ea14b3472bb6a5b
Juan Vargas <juandavc12@hotmail.com>
2020-11-10 22:41:31 -0500

    resuelto

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const Welcome = (props) =>{
+  return <h1>Hola {props.name}</h1>
+}
+
 class App extends Component {
   constructor() {
     super();
-
+    
     this.state = {
       names: ["Juan", "Pedro", "Germán"]
     }
   }
-
+  
   render() {
     return (
       <div>
         {this.state.names.map((name) =>
           <Welcome name={name} />
-        )}
+          )}
       </div>
     );
   }
